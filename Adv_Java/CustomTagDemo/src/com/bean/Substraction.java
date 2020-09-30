1f9d080bf5cbbb539d8d8c75eb27e66c19189638
Nivedita <shivaresnivedita@gmail.com>
2020-09-30 14:35:59 +0530

    custum tag

---
+++ Adv_Java/CustomTagDemo/src/com/bean/Substraction.java
@@ -0,0 +1,36 @@
+package com.bean;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class Substraction extends SimpleTagSupport{
+	int a,b;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+	
+	@Override
+	public void doTag() throws JspException, IOException {
+		JspWriter out=getJspContext().getOut();
+		int c=getA()-getB();
+		out.println("Substraction : "+c);
+	}
+	
+	
+}
